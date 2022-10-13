eb354fdfe807e4e44d2422413edf2a35f821f73d
Noah <iriaroan@naver.com>
2022-10-13 21:23:50 +0900

    Gallery : Listing image

--- Gallery/script/main.js
+++ Gallery/script/main.js
@@ -1,3 +1,21 @@
+const Gallery = {
+    initialize : () => {
+        Object.keys(imageData).map(name => {
+            const columnHeight = [1,2,3].map(i => document.querySelector(`#listFrame>.column:nth-child(${i})`).scrollHeight)
+            const nextColumn = columnHeight.indexOf(Math.min(...columnHeight))
+
+            const imageObject = document.createElement('img')
+            imageObject.src = `image/${name}.png`
+
+            const itemObject = document.createElement('div')
+            itemObject.className = 'item'
+            itemObject.appendChild(imageObject);
+
+            document.querySelector(`#listFrame>.column:nth-child(${nextColumn+1})`).appendChild(itemObject)
+        })
+    }
+}
+
 window.onload = () => {
-    console.log(imageData)
+    Gallery.initialize();
 }
