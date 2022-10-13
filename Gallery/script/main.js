a58173b30ba71b801bd3580994fdca0932f8bbd2
Noah <iriaroan@naver.com>
2022-10-13 21:46:06 +0900

    Gallery : Add function to view image

--- Gallery/script/main.js
+++ Gallery/script/main.js
@@ -8,11 +8,29 @@ const Gallery = {
             imageObject.src = `image/${name}.png`
 
             const itemObject = document.createElement('div')
+            itemObject.imageName = name
             itemObject.className = 'item'
+            itemObject.addEventListener('click', Gallery.viewImage);
             itemObject.appendChild(imageObject);
 
             document.querySelector(`#listFrame>.column:nth-child(${nextColumn+1})`).appendChild(itemObject)
         })
+    },
+
+    viewImage : (e) => {
+        const name = e.currentTarget.imageName
+        
+        document.querySelector("#blurFrame").classList.add("on")
+        
+        document.querySelector("#popFrame>.image").innerHTML = `<img src=image/${name}.png>`
+        document.querySelector("#popFrame>.date").innerHTML = imageData[name].date
+        document.querySelector("#popFrame>.desc").innerHTML = imageData[name].desc
+        document.querySelector("#popFrame").classList.add("on")
+    },
+
+    closeImage : () => {
+        document.querySelector("#blurFrame").classList.remove("on")
+        document.querySelector("#popFrame").classList.remove("on")
     }
 }
 
